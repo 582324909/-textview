2723810245d241f7f01229fee94b617f516ffa83
张伟伟 <zhangweiwei@choubeizudeMacBook-Pro.local>
2016-11-17 09:43:55 +0800

    Initial Commit

---
+++ 自适应textview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自适应textview
+//
+//  Created by 张伟伟 on 2016/11/17.
+//  Copyright © 2016年 张伟伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
